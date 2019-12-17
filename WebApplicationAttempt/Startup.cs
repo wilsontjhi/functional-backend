0e197d4c413c8aaf37471790ae0fa6d999e17ac2
Wilson Tjhi <wctjhi@illumina.com>
2019-12-17 15:37:25 +0800

    Allow cors

--- WebApplicationAttempt/Startup.cs
+++ WebApplicationAttempt/Startup.cs
@@ -25,6 +25,15 @@ namespace WebApplicationAttempt
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => {
+                        builder.AllowAnyOrigin();
+                        builder.AllowAnyMethod();
+                        builder.AllowAnyHeader();
+                    });
+            });
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
@@ -41,6 +50,7 @@ namespace WebApplicationAttempt
                 app.UseHsts();
             }
 
+            app.UseCors("CorsPolicy");
             app.UseHttpsRedirection();
             app.UseMvc();
         }
