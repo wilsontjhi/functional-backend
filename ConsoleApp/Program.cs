61a3be935b8be33b62508c2c7adcfbb3017eea8e
Wilson Tjhi <wctjhi@illumina.com>
2019-12-17 11:10:00 +0800

    Initial commit

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using DomainLogic;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine($"The only workflow is: {TopProductWorkflow.WorkflowName()}");
+        }
+    }
+}
---
+++ DomainLogic/TopProductWorkflow.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace DomainLogic
+{
+    public static class TopProductWorkflow
+    {
+        public static string WorkflowName() => "Top Product";
+    }
+}
