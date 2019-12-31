3f75256790227a3b775c8e42e6c904226fbc9d81
Wilson Tjhi <wctjhi@illumina.com>
2019-12-31 09:50:44 +0800

    Combining key summary and chart

--- WebApplicationAttempt/Controllers/ReportController.cs
+++ WebApplicationAttempt/Controllers/ReportController.cs
@@ -1,4 +1,8 @@
+using System.Linq;
+using DomainLogicExt.Data;
 using Microsoft.AspNetCore.Mvc;
+using static DomainLogicExt.Data.ProductRepository;
+using static DomainLogicExt.Estimator.SsaEstimator;
 
 namespace WebApplicationAttempt.Controllers
 {
@@ -24,5 +28,60 @@ namespace WebApplicationAttempt.Controllers
                 }
             });
         }
+
+        [HttpGet("{name}")]
+        public IActionResult Get(string name)
+        {
+            var productName = name.ToLower();
+            return
+                GetProduct(productName)
+                    .Match<IActionResult>(
+                        product => Ok(BuildSummaryAndChart(product)),
+                        error => NotFound()
+                    );
+        }
+
+        private object BuildSummaryAndChart(Product product)
+            =>
+                ProductWithData
+                    .Create(product)
+                    .Map(GetEstimate)
+                    .Match(
+                        estimate => PopulateDtoWithEstimate(product, estimate),
+                        error => PopulateDtoWithoutEstimate(product, error.ToString())
+                    );
+
+        private object PopulateDtoWithEstimate(Product product, float estimate)
+            => new
+            {
+                KeySummary = $"It is estimated that next month the sales value will be ${estimate}",
+                SalesData = BuildHistoricalChart(product)
+                                .SkipLast()
+                                .Concat(BuildEstimateChart(product, estimate))
+            };
+
+        private object PopulateDtoWithoutEstimate(Product product, string error)
+            => new
+            {
+                KeySummary = error,
+                SalesData = BuildHistoricalChart(product)
+            };
+
+        private SalesData[] BuildHistoricalChart(Product product)
+            => product.SalesData
+                .Select(s => new SalesData {Name = $"Month {s.Month}", Historical = s.Amount})
+                .ToArray();
+
+        private SalesData[] BuildEstimateChart(Product product, float? estimate = null)
+            => new[]
+                {
+                    new SalesData
+                    {
+                        Name = $"Month {product.SalesData.Last().Month}",
+                        Historical = product.SalesData.Last().Amount,
+                        Estimate = product.SalesData.Last().Amount
+                    },
+                    new SalesData {Name = $"Next Month", Estimate = estimate},
+                };
     }
 }
